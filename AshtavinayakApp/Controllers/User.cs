880b330c8335acf289dba7baac089e3d193fb2f5
Ganesh Mankar <ganeshnmankar28@gmail.com>
2026-09-13 02:35:22 +0530

    fix: add missing using Microsoft.AspNetCore.Authorization for [AllowAnonymous]

--- AshtavinayakApp/Controllers/User.cs
+++ AshtavinayakApp/Controllers/User.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
