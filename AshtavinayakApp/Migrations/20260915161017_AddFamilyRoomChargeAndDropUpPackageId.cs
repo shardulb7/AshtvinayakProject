cb0f3c6b0305832069ac8eb2628271a3a8b3c5a0
Ganesh Mankar <ganeshnmankar28@gmail.com>
2026-09-15 21:41:06 +0530

    fix: add EF Core migration for FamilyRoomChargePerPerson + DropUps.PackageId (fixes PendingModelChangesException crash)

---
+++ AshtavinayakApp/Migrations/20260915161017_AddFamilyRoomChargeAndDropUpPackageId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AshtavinayakAPP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFamilyRoomChargeAndDropUpPackageId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FamilyRoomChargePerPerson",
+                table: "Packages",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PackageId",
+                table: "DropUp",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FamilyRoomChargePerPerson",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "PackageId",
+                table: "DropUp");
+        }
+    }
+}
